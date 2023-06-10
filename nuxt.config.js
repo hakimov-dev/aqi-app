3aeebd9bded6ab412b5aeb7477f6f1abb65bc7f0
Muhammadamin <webhacker0100@gmail.com>
2023-06-10 20:42:42 +0500

    Download tailwind css

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    'ant-design-vue/dist/antd.css'
+    'ant-design-vue/dist/antd.css',
+    '@/assets/css/main.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -50,5 +51,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
   }
 }
