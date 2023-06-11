255562bcbc69a5e3febb85113d5ffe384569a491
Muhammadamin <webhacker0100@gmail.com>
2023-06-11 15:05:21 +0500

    Created new api base

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,10 @@ import axios from 'axios'
 
 export const api = axios
 
+export const aqiApi = axios
+
+aqiApi.defaults.headers.common['X-Api-Key'] = `${process.env.NUXT_ENV_AQI_KEY}`
+
 // Error handlers
 
 const errorHandler = (error: Object) => {
