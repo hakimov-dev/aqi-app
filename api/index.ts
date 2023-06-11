da0bbfc7fd1540ffd29d3ee717deb14777fc5742
Muhammadamin <webhacker0100@gmail.com>
2023-06-11 11:59:54 +0500

    Added new error handlers

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,18 @@
 import axios from 'axios'
 
 export const api = axios
+
+// Error handlers
+
+const errorHandler = (error: Object) => {
+    console.log('An error occurred:', error);
+};
+
+api.interceptors.response.use(
+    (response) => response.data,
+    (error) => {
+      errorHandler(error);
+      return Promise.reject(error);
+    }
+)
+  
